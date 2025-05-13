536dd40ca9a218fe36bdbb05172a4ef607c6f6aa
iliazodo <zodozdm@gmail.com>
2025-05-12 23:04:52 -0700

    text size changed.

--- frontend/src/components/homeComponents.jsx/Text.jsx
+++ frontend/src/components/homeComponents.jsx/Text.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Text = (props) => {
   return (
     <div className=" border-white border-2 w-5/6 mx-auto p-3 break-words">
-      <h2 className="text-4xl pixel-font">{props.title}</h2>
+      <h2 className="md:text-4xl text-2xl pixel-font">{props.title}</h2>
       <br />
       <p className="lg:text-xl">{props.children}</p>
     </div>
