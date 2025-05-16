b6b56aa9528844f842bb516e7c1bde6f8b209f63
iliazodo <zodozdm@gmail.com>
2025-05-15 19:51:19 -0700

    login for mobile fixed.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,17 +19,18 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center gap-5 h-full w-full bg-black">
+      <Link to="/">
       <img
         src="zodomixLogo.png"
         alt="zodomix logo"
         className="lg:w-2/3 md:w-3/4  mt-10"
       />
+      </Link>
       <form
         onSubmit={handleSubmit}
         className="w-full flex flex-col md:grid-cols-2 xl:w-2/3 items-center justify-center gap-3 z-10"
       >
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 md:mt-[calc(20%)]  items-center justify-center gap-3 mt-28 z-10">
-
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 md:mt-[calc(20%)]  items-center justify-center gap-3 mt-16 z-10">
           {/* HoneyPot */}
           <div className="absolute top-0 w-1 h-1 hidden">
             <label htmlFor="website">Leave this field empty</label>
@@ -46,7 +47,7 @@ const Login = () => {
           </div>
 
           <div className="w-5/6 flex flex-col  space-y-3 mx-auto">
-            <label className="text-3xl">USERNAME</label>
+            <label className="text-2xl">USERNAME</label>
             <input
               type="text"
               className="bg-transparent rounded-full p-5 text-2xl font-mono border-2 outline-none"
@@ -57,7 +58,7 @@ const Login = () => {
             />
           </div>
           <div className="w-5/6 flex flex-col  space-y-3 mx-auto">
-            <label className="text-3xl">PASSWORD</label>
+            <label className="text-2xl">PASSWORD</label>
             <input
               type="password"
               className="bg-transparent rounded-full p-5 text-2xl font-mono border-2 outline-none"
@@ -70,7 +71,7 @@ const Login = () => {
         </div>
         <button
           type="submit"
-          className={` bg-transparent border-2 rounded-full p-5 text-2xl w-1/2 transition duration-300 ease-out m-auto mt-20 ${
+          className={` bg-transparent border-2 rounded-full p-5 text-2xl w-1/2 transition duration-300 ease-out m-auto mt-14 ${
             !loading &&
             "hover:bg-white hover:text-black active:bg-black active:text-white"
           }  xl:w-1/4 cursor-pointer`}
@@ -85,9 +86,15 @@ const Login = () => {
       </form>
       <Link
         to="/signUp"
-        className="md:absolute bottom-5 right-5 text-3xl cursor-pointer transform duration-100 ease-in hover:font-bold xl:text-5xl sm:text-4xl"
+        className="md:absolute  bottom-5 right-5 text-3xl cursor-pointer transform duration-100 ease-in hover:font-bold xl:text-5xl sm:text-4xl"
       >
         SIGIN UP ⇨
+      </Link>
+      <Link
+        to="/signUp"
+        className="md:absolute bottom-5 left-5 text-3xl cursor-pointer transform duration-100 ease-in hover:font-bold xl:text-5xl sm:text-4xl"
+      >
+        ⇦ HOME
       </Link>
     </div>
   );
