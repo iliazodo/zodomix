d3069a7a2f3aa4d0a4f940bc5622bcac076deec7
iliazodo <zodozdm@gmail.com>
2025-06-10 19:55:03 -0700

    add react skeletone animation loader.

--- frontend/src/hooks/useGetGroups.js
+++ frontend/src/hooks/useGetGroups.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 
 
 const useGetGroups = () => {
-
+  const [loading , setLoading] = useState(false);
   const getGroups = async () => {
-
+    setLoading(true);
     try {
         const res = await fetch("/api/group/get" , {
             method: "GET",
@@ -16,10 +17,12 @@ const useGetGroups = () => {
 
     } catch (error) {
       console.log(error.message);
+    } finally{
+      setLoading(false);
     }
   };
 
-  return { getGroups };
+  return { loading ,getGroups };
 };
 
 export default useGetGroups;
--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -6,9 +6,10 @@ import useGetGroups from "../hooks/useGetGroups.js";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext.jsx";
 import toast from "react-hot-toast";
+import ContentLoader from "react-content-loader";
 
 const Explore = () => {
-  const { getGroups } = useGetGroups();
+  const { loading, getGroups } = useGetGroups();
 
   const [groups, setGroups] = useState([]);
   const [searchInput, setSearchInput] = useState();
@@ -28,6 +29,24 @@ const Explore = () => {
       toast.error("PLEASE LOGIN OR SIGNUP");
     }
   };
+
+  // Group loader skeletone
+
+  const GroupLoader = () => (
+    <ContentLoader
+      speed={2}
+      width={350}
+      height={160}
+      viewBox="0 0 350 160"
+      backgroundColor="#2c2c2c"
+      foregroundColor="#999"
+      className="mx-auto"
+    >
+      <rect x="0" y="0" rx="12" ry="12" width="350" height="100" />
+      <rect x="0" y="110" rx="6" ry="6" width="220" height="15" />
+      <rect x="0" y="135" rx="6" ry="6" width="180" height="15" />
+    </ContentLoader>
+  );
 
   useEffect(() => {
     handleGetGroups();
@@ -54,7 +73,11 @@ const Explore = () => {
           </button>
         </div>
         <div className="overflow-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pb-32 pt-10 lg:pb-20 lg:pt-10 gap-16">
-          {!searchInput
+          {loading
+            ? Array(6)
+                .fill(0)
+                .map((_, i) => <GroupLoader key={i} />)
+            : !searchInput
             ? groups.map((group) => (
                 <Group
                   key={group._id}
